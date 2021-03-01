2442cbbb10fb2cd576d08366576dcfc11e7c41e3
choiwwsx <choiwsx@nate.com>
2021-03-01 13:27:48 +0900

    2021-03-01리액트 추가중

--- WheelOfFortune-master/src/main/java/com/example/demo/DemoApplication.java
+++ WheelOfFortune-master/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +11,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
+
 
 }
--- WheelOfFortune-master/src/test/java/com/example/demo/FortuneTest.java
+++ WheelOfFortune-master/src/test/java/com/example/demo/FortuneTest.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.example.demo.domain.Fortune;
 import com.example.demo.repository.FortuneRepository;
 import com.example.demo.service.MockFortuneService;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -47,5 +48,19 @@ public class FortuneTest {
 
         assertThat(fortune1.equals(fortune2));
 
+    }
+
+    @Test
+    public void run() throws Exception {
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword("test"); //2번 설정의 암호화 키를 입력
+
+        String enc = pbeEnc.encrypt("1234"); //암호화 할 내용
+        System.out.println("enc = " + enc); //암호화 한 내용을 출력
+
+        //테스트용 복호화
+        String des = pbeEnc.decrypt(enc);
+        System.out.println("des = " + des);
     }
 }
