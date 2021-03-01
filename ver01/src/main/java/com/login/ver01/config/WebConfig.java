6fcf8b4e76a27eaafecd5ff9705aedb7e75f537b
choiwwsx <choiwsx@nate.com>
2021-03-01 15:24:22 +0900

    2021-0301 리액트 설치중

--- ver01/src/main/java/com/login/ver01/config/WebConfig.java
+++ ver01/src/main/java/com/login/ver01/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000/");
     }
 }
--- ver01/src/main/java/com/login/ver01/controller/ApiController.java
+++ ver01/src/main/java/com/login/ver01/controller/ApiController.java
@@ -1,11 +1,13 @@
 package com.login.ver01.controller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class ApiController {
 
