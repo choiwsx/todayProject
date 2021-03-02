58ec57808f5c8a9ae16b099bc30f9cc0071cfec6
Wonseok Choi <choiwsx@nate.com>
2021-03-02 22:30:32 +0900

    2021-03-02 리액트 프론트가 제대로 푸쉬가 안된듯 함.

--- showChart/src/main/java/com/coinchart/show/repository/CoinRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coinchart.show.repository;
-
-import com.coinchart.show.domain.Coin;
-
-import java.util.List;
-
-public interface CoinRepository {
-    List<Coin> findByQuery(String symbol);
-}
